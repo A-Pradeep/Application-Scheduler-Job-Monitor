8e3634a3389e21f7e1ce014155ac8c778ec1f090
A-Pradeep <sharmapradeep872@gmail.com>
2021-02-02 23:41:21 +0530

    Created DB connection

---
+++ DB/mongoose.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Check DB connection
+const db = mongoose.connection;
+db.on("error", (err) => console.error(err));
+db.once("open", () => console.log("Connected to Database"));
