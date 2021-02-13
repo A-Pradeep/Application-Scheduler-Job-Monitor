bf6df846b48011fe7439479e083de86cd5531cc4
A-Pradeep <sharmapradeep872@gmail.com>
2021-02-14 00:24:13 +0530

    Added Logs, errorCount for the Job Model

--- Models/jobModel.js
+++ Models/jobModel.js
@@ -3,10 +3,18 @@ const mongoose = require("mongoose");
 const jobSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
+    currentStatus: { type: String, required: true },
     method: { type: String, required: true },
     frequency: { type: String, required: true },
     alertType: { type: String, required: true },
     endpoint: { type: String, required: true },
+    errorCount: { type: Number },
+    logs: [
+      {
+        status: Number,
+        message: String,
+      },
+    ],
   },
   { timestamps: true }
 );
