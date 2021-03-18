2dc852d5ea67b0d589952632862dd5692751d366
A-Pradeep <sharmapradeep872@gmail.com>
2021-03-18 20:11:00 +0530

    Initial Dashboard component.

---
+++ client/src/Components/Dashboard.jsx
@@ -0,0 +1,14 @@
+import { Jumbotron } from "react-bootstrap";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Jumbotron>
+        <h1>Welcome to Application Scheduler & Job Monitor.</h1>
+        <p>Let's get started.</p>
+      </Jumbotron>
+    </div>
+  );
+};
+
+export default Dashboard;
