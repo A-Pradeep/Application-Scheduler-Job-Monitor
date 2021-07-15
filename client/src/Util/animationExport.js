b63ea4b6fbc73e479f6443e679c2373e5a55faf9
A-Pradeep <sharmapradeep872@gmail.com>
2021-07-16 01:47:57 +0530

    Default Animation Icon export file. All animation icons under one file.

---
+++ client/src/Util/animationExport.js
@@ -0,0 +1,23 @@
+import activity from "react-useanimations/lib/activity";
+import alertCircle from "react-useanimations/lib/alertCircle";
+import arrowRightCircle from "react-useanimations/lib/arrowRightCircle";
+import arrowLeftCircle from "react-useanimations/lib/arrowLeftCircle";
+import share from "react-useanimations/lib/share";
+import loading from "react-useanimations/lib/loading";
+import loading2 from "react-useanimations/lib/loading2";
+import loading3 from "react-useanimations/lib/loading3";
+import trash2 from "react-useanimations/lib/trash2";
+
+const parentAnimation = {
+  activity,
+  alertCircle,
+  arrowRightCircle,
+  arrowLeftCircle,
+  share,
+  loading,
+  loading2,
+  loading3,
+  trash2,
+};
+
+export default parentAnimation;
