e0aca2782b1657663e765fe543779d1eba8dc15e
A-Pradeep <sharmapradeep872@gmail.com>
2021-07-21 02:00:06 +0530

    Refactor Code. Removed repeated code, created constant NotifyParams.

--- client/src/Components/Common/Notify.jsx
+++ client/src/Components/Common/Notify.jsx
@@ -1,8 +1,9 @@
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-const notifySuccess = (title, position = "top-right") =>
-  toast.success(title, {
+// Constant Notification parameters
+const NotifyParams = (position = "top-right") => {
+  return {
     position: position,
     autoClose: 5000,
     hideProgressBar: false,
@@ -10,29 +11,17 @@ const notifySuccess = (title, position = "top-right") =>
     pauseOnHover: true,
     draggable: true,
     progress: undefined,
-  });
+  };
+};
 
-const notifyDanger = (title, position = "top-right") =>
-  toast.warn(title, {
-    position: position,
-    autoClose: 5000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-  });
+const notifySuccess = (title, position) =>
+  toast.success(title, NotifyParams(position));
 
-const notifyError = (title, position = "top-right") =>
-  toast.error(title, {
-    position: position,
-    autoClose: 5000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-  });
+const notifyDanger = (title, position) =>
+  toast.warn(title, NotifyParams(position));
+
+const notifyError = (title, position) =>
+  toast.error(title, NotifyParams(position));
 
 const Notify = {
   success: notifySuccess,
