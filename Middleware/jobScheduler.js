1eb2f46456c1c76942e867080f4451ddfbcc74d6
A-Pradeep <sharmapradeep872@gmail.com>
2021-07-25 01:11:37 +0530

    Job scheduler. Options to create, stop, restart, delete job.

--- Middleware/jobScheduler.js
+++ Middleware/jobScheduler.js
@@ -1,37 +1,121 @@
+const { http } = require("../Middleware/http");
 const cronManager = require("cron-job-manager");
+const logModel = require("../Models/logModel");
+
 const job = new cronManager();
+
 class jobScheduler {
+  // Get Current Jobs
+  getScheduleJobs() {
+    var data = job.listCrons();
+    return data;
+  }
+
   // Create new Job
-  scheduleNewJob(id) {
-    console.log(`Hey Welcome Ps - ${id}`);
+  scheduleNewJob({ method, endpoint, frequency }, id) {
     job.add(
       id,
-      "* * * * *",
+      frequency,
       async () => {
-        console.log("tick - what should be executed?");
+        switch (method) {
+          case "GET":
+            await http
+              .get(endpoint)
+              .then(({ status, statusText, config }) => {
+                // Save log
+                const newLog = new logModel({
+                  jobID: id,
+                  status: status,
+                  endpoint: config.url,
+                  method: config.method,
+                  message: statusText,
+                });
+
+                try {
+                  newLog.save();
+                } catch (error) {
+                  console.log(error);
+                }
+              })
+              .catch((error) => {
+                const newLog = new logModel({
+                  jobID: id,
+                  status: error.errno,
+                  endpoint: error.config.url,
+                  method: error.config.method,
+                  message: error.code,
+                });
+
+                try {
+                  newLog.save();
+                } catch (error) {
+                  console.log(error);
+                }
+              });
+            break;
+          case "DELETE":
+            await http
+              .delete(endpoint)
+              .then(({ status, statusText, config }) => {
+                // Save log
+                const newLog = new logModel({
+                  jobID: id,
+                  status: status,
+                  endpoint: config.url,
+                  method: config.method,
+                  message: statusText,
+                });
+
+                try {
+                  newLog.save();
+                } catch (error) {
+                  console.log(error);
+                }
+              })
+              .catch((error) => {
+                const newLog = new logModel({
+                  jobID: id,
+                  status: error.errno,
+                  endpoint: error.config.url,
+                  method: error.config.method,
+                  message: error.code,
+                });
+
+                try {
+                  newLog.save();
+                } catch (error) {
+                  console.log(error);
+                }
+              });
+            break;
+        }
       },
       {
         start: true,
-        onComplete: () => {
-          console.log(`${id} has stopped....`);
-        },
       }
     );
-    console.log(`\nI got the current jobs: ${job}`);
   }
 
   // Stop existing job
   stopScheduleJob(id) {
-    console.log(`Stopping Job : ${id}`);
-    console.log(job.exists(id) ? "key exists" : "Job not found.");
     if (job.exists(id)) job.stop(id);
+  }
+
+  // Stop all existing job
+  stopAllScheduleJob() {
+    job.stopAll();
   }
 
   // Stop existing job
   reStartScheduleJob(id) {
-    console.log(`Starting Job : ${id}`);
-    console.log(job.exists(id) ? "key exists" : "Job not found.");
     if (job.exists(id)) job.start(id);
+  }
+
+  // Delete existing Job
+  deleteScheduleJob(id) {
+    if (!job.exists(id)) return false;
+    job.deleteJob(id);
+    return true;
   }
 }
 
